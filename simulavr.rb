92001509a1ebbebbeaa5d2cb63be40db133180e0
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-02-27 19:52:11 -0330

    Switch to head-only simulavr build.
    The 1.0.0 release is quite broken: it relies on libbfd (which is supposed
    to be included as source rather than linked against) and other special
    behaviour that is fixed in HEAD.

--- simulavr.rb
+++ simulavr.rb
@@ -2,10 +2,7 @@ require 'formula'
 
 class Simulavr < Formula
   homepage 'http://www.nongnu.org/simulavr/'
-  version '1.0.0'
-  url "http://download.savannah.gnu.org/releases/simulavr/simulavr-#{version}.tar.gz"
   head 'https://github.com/jpommerening/simulavr.git'
-  sha1 'e7cacc74be974793bd9c18330ec8d128fbd17d42'
 
   depends_on 'avr-binutils'
   depends_on 'avr-libc'
@@ -15,8 +12,6 @@ class Simulavr < Formula
     depends_on 'autoconf'
     depends_on 'automake'
     depends_on 'swig'
-  else
-    depends_on 'swig-13'
   end
 
   def patches
@@ -24,25 +19,15 @@ class Simulavr < Formula
 
     if build.head?
       [ 'file://' + patchdir + '/patches/simulavr-HEAD-gtest.patch' ]
-    else
-      [ 'file://' + patchdir + '/patches/simulavr-1.0.0-resize.patch',
-        'file://' + patchdir + '/patches/simulavr-1.0.0-avrgcc4.9.2-darwin.patch' ]
     end
   end
 
   def install
-    multios = `gcc --print-multi-os-directory`.chomp
-    osrelease = `/usr/sbin/sysctl -n kern.osrelease`.chomp
-    binutils = Formula.factory('avr-binutils')
-
     if build.head?
       system "./bootstrap"
     end
 
-    system "./configure", "--with-bfd=#{binutils.opt_prefix}/#{multios}-apple-darwin#{osrelease}/avr",
-                          "--with-libiberty=#{binutils.opt_prefix}/lib/#{multios}",
-                          "--prefix=#{prefix}",
-                          "LDFLAGS=-lz"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
